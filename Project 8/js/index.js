f9a3249c60ce11c7ec6e7cef78e2c5d45e261b66
Steve12309 <phamtrannhata@gmail.com>
2024-02-08 11:02:42 +0700

    Full HomeWork Project

--- Project 8/js/index.js
+++ Project 8/js/index.js
@@ -3,6 +3,8 @@ var imgOutput = document.getElementById("imgOut");
 var imgOutput2 = document.getElementById("imgOut2");
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
+var canvas1 = document.getElementById("canvas-download");
+var context1 = canvas1.getContext("2d");
 var downloadBtn = document.getElementById("download");
 var load = false;
 var reloadBtn = document.getElementById("reload");
@@ -12,14 +14,11 @@ var leftE = document.getElementById("left");
 var frameAlready = document.querySelectorAll(".already");
 var count = 1;
 var count1 = frameAlready.length + 1;
-var canvas1 = document.getElementById("canvas-download");
-var context1 = canvas1.getContext("2d");
-var frameEs = document.querySelectorAll("#frameOut");
-var frameBorders = document.querySelectorAll("#border");
 function selectFrame(selectedIndex) {
   if (load === false) {
     alert("Please upload your avatar before choose frame to add");
   } else {
+    var frameEs = document.querySelectorAll("#frameOut");
     frameEs.forEach(function (frame, index) {
       if (index + 1 == selectedIndex) {
         context.globalAlpha = 1;
@@ -31,6 +30,7 @@ function selectFrame(selectedIndex) {
       } else {
       }
     });
+    var frameBorders = document.querySelectorAll("#border");
     frameBorders.forEach(function (frameBorder, index) {
       if (index + 1 === selectedIndex) {
         frameBorder.classList.remove("display");
@@ -99,9 +99,12 @@ downloadBtn.onclick = function () {
   if (load === false) {
     alert("Can not find avatar or avatar with frame to download");
   } else {
+    var link = document.createElement("a");
     var previewCanvas = canvas1.toDataURL();
-    downloadBtn.download = "pic_frame.png";
-    downloadBtn.href = previewCanvas;
+    link.href = previewCanvas;
+    link.download = "pic_frame.png";
+    link.style.display = "none";
+    link.click();
   }
 };
 
@@ -114,10 +117,35 @@ input.onchange = function (e) {
   reader.onload = function (e) {
     var img = new Image();
     img.onload = function () {
-      canvas1.width = img.naturalWidth;
-      canvas1.height = img.naturalHeight;
-      context1.globalAlpha = 1;
-      context1.drawImage(imgOutput, 0, 0, canvas1.width, canvas1.height);
+      var imgWidth = img.naturalWidth;
+      var imgHeight = img.naturalHeight;
+      var sourceX = imgWidth / 4;
+      var sourceY = 0;
+      var imglength = sourceX + sourceX;
+      var result = 0;
+      if (imgWidth > imgHeight) {
+        result = imgHeight;
+      } else {
+        result = imgWidth;
+      }
+      canvas1.width = result;
+      canvas1.height = result;
+      if (imgWidth == imgHeight) {
+        context1.globalAlpha = 1;
+        context1.drawImage(imgOutput, 0, 0, canvas1.width, canvas1.height);
+      } else {
+        context1.drawImage(
+          imgOutput2,
+          sourceX,
+          sourceY,
+          imglength,
+          imgHeight,
+          0,
+          0,
+          imgWidth,
+          imgHeight
+        );
+      }
     };
     img.src = e.target.result;
   };
